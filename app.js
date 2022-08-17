438b0d1202087811ab49385663e1579f0c8b7116
Intesar Haque <xyz@gmail.com>
2022-08-17 18:17:42 +0600

    screenshare

--- app.js
+++ app.js
@@ -8,14 +8,23 @@ const io = require('socket.io')(server, {
 });
 
 io.on('connection', socket => {
-    socket.on('join-room', (roomId, userId) => {
+    socket.on('join-room', (roomId, connectedUser) => {
         socket.join(roomId);
-        socket.broadcast.to(roomId).emit('user-connected', userId);
+        socket.broadcast.to(roomId).emit('user-connected', connectedUser);
         socket.on('disconnect', () => {
-            socket.broadcast.to(roomId).emit('user-disconnected', userId);
+            socket.broadcast.to(roomId).emit('user-disconnected', connectedUser.peerId);
         })
         socket.on('chat', (content) => {
             socket.broadcast.to(roomId).emit('new-message', content);
+        })
+        socket.on('draw', (content) => {
+            socket.broadcast.to(roomId).emit('new-draw', content);
+        })
+        socket.on('white-board', (content) => {
+            socket.broadcast.to(roomId).emit('hide-whiteboard', content);
+        })
+        socket.on('share-screen', (content) => {
+            socket.broadcast.to(roomId).emit('on-share-screen', content);
         })
     })
 
